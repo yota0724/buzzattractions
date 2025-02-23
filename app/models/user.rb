86e9509ba6772fa8c3991f19ce3c56d18385dfee
EC2 Default User <ec2-user@ip-172-31-43-218.ap-northeast-1.compute.internal>
2025-02-23 11:59:29 +0000

    2ndデプロイ+テスト完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,6 @@ class User < ApplicationRecord
   end
   
   def get_profile_image(width, height)
-    
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
